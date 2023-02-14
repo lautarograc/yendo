441b9c92cc1c138fabf2b06fb74ebec92f306966
lautarograc <lautaro@graciani.ar>
2023-02-14 16:31:00 -0300

    Fix user controller, add flash devise messages on layout, fix compatibility problem for :delete on devise actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'static_pages#landing'
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
+  # Allows us to use link_to for session destroy
+  devise_scope :user do
+    get "/users/sign_out", as: "sign_out", to: "devise/sessions#destroy"
+  end
 end
