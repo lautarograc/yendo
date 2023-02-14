7c30c995959211341a469c8e9533426c9fddfc71
lautarograc <lautaro@graciani.ar>
2023-02-14 16:08:45 -0300

    Added various fixes to main for deployment issues

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -5,3 +5,4 @@
 
 //= link navbar.css
 //= link landing.css
+//= link application.css
--- db/migrate/20230206150215_create_addresses.rb
+++ db/migrate/20230206150215_create_addresses.rb
@@ -2,7 +2,6 @@ class CreateAddresses < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
       t.string :street, null: false
-      t.integer :zip, null: false
       t.references :store, null: false, foreign_key: true
 
       t.timestamps
