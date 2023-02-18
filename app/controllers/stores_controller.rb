d3f5a2408a74b92c40659b776b2922acce552c78
lautarograc <lautaro@graciani.ar>
2023-02-18 09:34:08 -0300

    create search box and category filter feature

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -2,22 +2,26 @@ class StoresController < ApplicationController
     before_action :set_store, only: %i[show]
 
     def index
-
-        if params[:name].present? #si encuentra parametros de :name mostrará los que se correspondan
-            @stores = Store.where("name LIKE ?", Store.sanitize_sql_like(params[:name]) + "%")
-            render json: @stores, only: [:name, :category_id], include: {address: {only: :street}}
+        @categories = Category.all
+        if params[:search]
+            @stores = Store.includes(:foods).where("stores.name ILIKE :search OR foods.name ILIKE :search", search: "%#{params[:search]}%").references(:foods)
+            
+        # if params[:name].present? #si encuentra parametros de :name mostrará los que se correspondan
+        #     @stores = Store.where("name LIKE ?", Store.sanitize_sql_like(params[:name]) + "%")
+        #     render json: @stores, only: [:name, :category_id], include: {address: {only: :street}}
         elsif params[:category_id].present? #si encuentra parametros de categoria mostrará los que se corresponda
             @stores = Store.where("category_id = ?", params[:category_id])
-            render json: @stores, only: [:name, :category_id] ,  include: {address: {only: :street}}
-        elsif params[:near].present?
-            if params[:near] == "true"
-                distance_order(Address.second)
-            end
+        #     render json: @stores, only: [:name, :category_id] ,  include: {address: {only: :street}}
+        # elsif params[:near].present?
+        #     if params[:near] == "true"
+        #         distance_order(Address.second)
+        #     end
         else
-            @stores = Store.all#json: Store.all, only: [:name, :category_id], include: {address: {only: :street}}
+            @stores = Store.all
         end
     end
 
+    
     def show
         render json: @store, :include => [:address, :foods], status: :ok
     end
@@ -37,5 +41,9 @@ class StoresController < ApplicationController
         @store = Store.find(params[:id])
     rescue ActiveRecord::RecordNotFound
         render json: { error: "Could not find any store with ID '#{params[:id]}'" }
+    end
+
+    def stores_params
+        params.require(:store).permit(:search)
     end
 end
