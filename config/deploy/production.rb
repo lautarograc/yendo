0cee4a325e9d102d2812de9afb32ed1f22284c86
lautarograc <lautarograciani2106@gmail.com>
2023-02-22 01:28:19 -0300

    added context for new deployment

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 
 #FIXME replace 1.2.3.4 with your IP address
-server '100.25.177.176', user: 'deploy', roles: %w{web app db}
+server '100.25.177.176', user: 'deploy', roles: %w{app db web}
 set :rails_env, 'production'
