2b82ebcb3cdbb2827b6eac5b0db349717568f0ce
lautarograc <lautarograciani2106@gmail.com>
2023-02-23 17:48:30 -0300

    minor fixes

--- app/controllers/shopping_carts_controller.rb
+++ app/controllers/shopping_carts_controller.rb
@@ -2,14 +2,14 @@ class ShoppingCartsController < ApplicationController
     before_action :authenticate_user!
   
     def show
-      @shopping_cart = current_user.shopping_cart
+      @shopping_cart = current_user.current_cart
     end
   
     def add_food
-      @shopping_cart = current_user.shopping_cart
+      @shopping_cart = current_user.current_cart
       food = Food.find(params[:food_id])
       quantity = params[:quantity] || 1
-      if current_user.shopping_cart.nil?
+      if current_user.current_cart.nil?
         ShoppingCart.create!(user: current_user)
       end
       @shopping_cart.add_food(food, quantity)
@@ -17,7 +17,7 @@ class ShoppingCartsController < ApplicationController
     end
   
     def checkout
-      @shopping_cart = current_user.shopping_cart
+      @shopping_cart = current_user.current_cart
       @shopping_cart.checkout
       redirect_to orders_path
     end
