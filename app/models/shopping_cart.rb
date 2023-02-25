b7138f7807a56e44d30975952284a657b67a63f6
lautarograc <lautarograciani2106@gmail.com>
2023-02-24 22:30:15 -0300

    Revert "Styles"

--- app/models/shopping_cart.rb
+++ app/models/shopping_cart.rb
@@ -1,21 +1,3 @@
-# == Schema Information
-#
-# Table name: shopping_carts
-#
-#  id          :bigint           not null, primary key
-#  total_price :decimal(10, 2)   default(0.0)
-#  created_at  :datetime         not null
-#  updated_at  :datetime         not null
-#  user_id     :bigint           not null
-#
-# Indexes
-#
-#  index_shopping_carts_on_user_id  (user_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (user_id => users.id)
-#
 class ShoppingCart < ApplicationRecord
   belongs_to :user
   has_many :line_items
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,20 +1,3 @@
-# == Schema Information
-#
-# Table name: orders
-#
-#  id         :bigint           not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  user_id    :bigint           not null
-#
-# Indexes
-#
-#  index_orders_on_user_id  (user_id)
-#
-# Foreign Keys
-#
-#  fk_rails_...  (user_id => users.id)
-#
 require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
