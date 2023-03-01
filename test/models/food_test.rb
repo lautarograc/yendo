6395978f99995597d965244eea3c90e750f47656
lautarograc <lautaro@graciani.ar>
2023-03-01 16:05:46 -0300

    various fixes

--- test/models/food_test.rb
+++ test/models/food_test.rb
@@ -2,13 +2,15 @@
 #
 # Table name: foods
 #
-#  id         :bigint           not null, primary key
-#  name       :string           not null
-#  price      :integer          not null
-#  url        :string
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  store_id   :bigint           not null
+#  id                :bigint           not null, primary key
+#  name              :string           not null
+#  price             :integer          not null
+#  url               :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  store_id          :bigint           not null
+#  stripe_price_id   :string
+#  stripe_product_id :string
 #
 # Indexes
 #
