5401186b3c520f38ae7bf360f40d40f95b09a4ab
lautarograc <lautaro@graciani.ar>
2023-03-01 15:04:22 -0300

    change ip to elastic one

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
 
 #FIXME replace 1.2.3.4 with your IP address
-server '100.25.177.176', user: 'deploy', roles: %w{app db web}
+server '3.222.22.62', user: 'deploy', roles: %w{app db web}
 set :rails_env, 'production'
