3b02ad0ffef67ebf3ad00b5a3912cf0bddb1b5cb
lautarograc <lautarograciani2106@gmail.com>
2023-03-02 16:22:58 -0300

    Remove unused code

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -8,39 +8,7 @@ class StoresController < ApplicationController
         @stores = @stores.filter_by_category(params[:category_id]) if params[:search].present? && params[:category_id].present?
         @stores = @stores.where("category_id = ?", params[:category_id]) if params[:category_id].present?
         @stores = @stores.order(average: :desc) if params[:ratings].present?
-        #     @stores = Store.includes(:foods).where("stores.name ILIKE :search OR foods.name ILIKE :search", search: "%#{params[:search]}%").references(:foods)
-        #     if params[:category_id].present?
-        #         @stores = Store.includes(:foods).where("stores.name ILIKE :search OR foods.name ILIKE :search", search: "%#{params[:search]}%").references(:foods).where("category_id = ?", params[:category_id])
-        #         if params[:ratings].present?
-        #             @stores = @stores.order(average: :desc)
-        #         end
-        #     elsif params[:ratings].present?
-        #         if params[:category_id].present?
-        #             @stores = Store.includes(:foods).where("stores.name ILIKE :search OR foods.name ILIKE :search", search: "%#{params[:search]}%").references(:foods).where("category_id = ?", params[:category_id])
-        #         end    
-        #         @stores = @stores.order(average: :desc)
-        #     end
-        # elsif params[:ratings].present?
-        #     @stores = Store.all
-        #     if params[:category_id].present?
-        #         @stores = Store.includes(:foods).where("stores.name ILIKE :search OR foods.name ILIKE :search", search: "%#{params[:search]}%").references(:foods).where("category_id = ?", params[:category_id])
-        #     end
-        #     @stores = @stores.order(average: :desc)
-        # elsif params[:category_id].present? #si encuentra parametros de categoria mostrará los que se corresponda
-        #     @stores = Store.where("category_id = ?", params[:category_id])
-        #     if params[:ratings].present?
-        #         @stores = @stores.order(average: :desc)
-        #     end
-        # elsif params[:near].present?
-        #     if params[:near] == "true"
-        #         distance_order(Location.second)
-        #     end
-        # else
-            # @stores = Store.all
-            # if params[:ratings].present?
-            #     @stores = @stores.order(average: :desc)
-            # end
-        # end
+        
     end
     
     def show 
@@ -50,20 +18,15 @@ class StoresController < ApplicationController
 
     end
 
-    def distance_order(location_object) # Al pasar un objeto de Location nos devuelve los locales dentro del radio especificado
+    # Al pasar un objeto de Location nos devuelve los locales dentro del radio especificado
+    def distance_order(location_object) 
         @locations = location_object.nearbys(5)
         @stores = []
         @locations.each do |obj|
-            @stores << Store.where(id: obj.store_id)#, only: [:name, :category_id], include: {address: {only: :street}}
+            @stores << Store.where(id: obj.store_id)
         end
         render json: @stores, only: [:name, :category_id] ,  include: {location: {only: :street}}
     end
-
-    def avg
-        @avg = (store.ratings.sum.to_f/store.ratings.size)
-    end
-
-    helper_method :avg
 
     private
     
